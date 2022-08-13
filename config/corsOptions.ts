3c0fa7ed51e5bb2f7a6de9db0129d486ae8ca544
symeon2-12 <szymongreszta@interia.pl>
2022-08-14 00:53:05 +0200

    added netlify site cors

--- config/corsOptions.ts
+++ config/corsOptions.ts
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
+  "https://szkolkakrzelowscy.netlify.app",
 ];
 
 const corsOptions: cors.CorsOptions = {
